347017a3c27e814f5750e4ebc3ab783b135e0db3
moises <gmoises926@gmail.com>
2020-03-17 17:15:33 -0600

    Mecanicas de juego core 2 version 1

--- WebContent/Pages/Player.js
+++ WebContent/Pages/Player.js
@@ -2,60 +2,78 @@
  * 
  * @fileoverview Este archivo contiene la Clase player con sus métodos.
  * @author Moisés
- * @version 0.1.0
+ * @version 0.1.1
  */
 
 
  /**
   *@class Player, objeto que administra la información del jugador
   */
-function Player(){
-    /** @type {boolean} Determina si el jugador se encuentra de turno o no.*/
-    this.onPlay = null;
+function Player(name=null,id = null,hand = new Hand()){
 
     /** @type {string} Identificador del jugador */
-    this.ID = null;
+    this.id = id;
 
     /** @type {string} Nombre del jugador */
-    this.name = null;
+    this.name = name;
 
-    /**@type {list}  Lista que guarda las cartas que el jugador tiene en la mano
-     * cada elemento debe ser @type {string} de la forma "color_figura"
+    /**
+     * @type {Hand} Objeto que administra las cartas del jugador.
     */
-    this.hand = [];
-    
-    /**@method setID Establece el ID del jugador
-     * @param {Integer} charactersNumber determina el número de caracteres
-     * que debe tener el ID
+    this.hand = hand;
+
+
+    /**@method takeCard añade una carta a la mano del jugador
+     * 
      */
-    this.setID = function(charactersNumber){
-        random = new RandomGenerator()
-        this.ID = random.randomAlpha(10);
+    this.takeCard = function(){
+        return this.hand.takeCard();
     }
 
-    /**@method setName Establece el nombre del jugador.
-     * @param {string} name nombre del jugador.
-     */
-    this.setName = function(name){
-        this.name = name;
-    }
-
-    /**@method addCard añade una carta a la mano del jugador
-     * @param  {string} card es la carta a añadir.
-     */
-    this.addCard = function(card){
-        this.hand.push(card);
-    }
-
-    /**@method dropCard suelta o saca una carta de la mano del jugador */
+    /**@method dropCard suelta o saca una carta de la mano del jugador.*/
     this.dropCard = function(card){
-        var tempHand = [];
-        this.hand.forEach(element => {
-            if(element != card){
-                tempHand.push(element)
-            }
-        });
-        this.hand = tempHand;
+        return this.hand.dropCard(card);
+    }
+    
+    /**@method drawTwo Toma las dos primeras cartas de la baraja. */
+    this.drawTwo = function(){
+        for (let i = 0; i < 2; i++) {
+			this.hand.takeCard();
+		}
+    }
+    /**@method drawFour Toma las cuatro primeras cartas de la baraja. */
+    this.drawFour = function(){
+        for (let i = 0; i < 4; i++) {
+			this.hand.takeCard();
+		}
+    }
+    /**@method drawSix Toma las seis primeras cartas de la baraja. */    
+    this.drawSix = function(){
+        for (let i = 0; i < 6; i++) {
+			this.hand.takeCard();
+		}
+    }
+
+    /**
+     * @method toString Crea una representación JSON del objeto.
+     * @param tab Cantidad de tabs que tendrá al inicio.
+     * @return JSON del objeto.
+     */
+    this.toString = function(tab = 0){
+        var tabs = "\t".repeat(tab);
+        var result = `${tabs}{\n`
+        result += `${tabs}\t\t\"id\":${this.id},\n`
+        result += `${tabs}\t\t\"name\":\"${this.name}\",\n`
+        result += `${tabs}\t\t\"hand\":\n${this.hand.toString(tab+1)}\n`
+        result += `${tabs}}`
+
+        return result;
+    }
+    
+    this.parse = function(json){
+    	this.id = json["id"];
+    	this.name = json["name"];
+    	this.hand.parse(json["hand"]);
     }
 
 }
--- WebContent/Pages/gameMaker.jsp
+++ WebContent/Pages/gameMaker.jsp
@@ -1,20 +1,46 @@
-<%@page import="core.Hand"%>
-<%@page import="core.Player"%>
-<%@page import="core.Game"%>
-<%@page import="core.Deck"%>
-<%@page import="clases.FileManager"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%
-	Game game = new Game(
-			request.getParameter("gameID").toString().strip(),
-			new Player(request.getParameter("hostPlayer").toString().trim(), new Hand()),
-			new Player(request.getParameter("guestPlayer").toString().trim(), new Hand())
-			);
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Game maker</title>
+<script src="../jquery-3.4.1.min.js"></script>
+<script src = "RandomGenerator.js"></script>
+	<script src = "LinkedList.js"></script>
+	<script src = "EValue.js"></script>
+	<script src = "EColor.js"></script>
+	<script src = "Card.js"></script>
+	<script src = "Deck.js"></script>
+	<script src = "DiscardPile.js"></script>
+	<script src = "Player.js"></script>
+	<script src = "Game.js"></script>
+	<script src = "Hand.js"></script>
+</head>
+<body>
+<% out.print(String.format("<input type='hidden' id='gameId' value='%s'>",request.getParameter("gameId"))); %>
+<% out.print(String.format("<input type='hidden' id='name' value='%s'>",request.getParameter("name"))); %>
+<script>
+	var id = document.querySelector("#gameId").value;
+	var name = document.querySelector("#name").value;
+	$.get("getContent.jsp",{"file":"logIn.json"},function(data){
+		data = JSON.parse(data.trim());
+		playersList = data.players;
+		
+		var players = new LinkedList();
+		
+		for ( var index in data.players) {
+			players.add(new Player(data.players[index]));
+		}
+		var game = new Game(id,players);
+		game.generate();/**Genera la partida.*/
+		console.log(game.toString());
+		$.get("write.jsp",{"file":"game.json","content":game.toString(),"override":true},function(){
+			window.location = `UNO.jsp?name=\${name}`;
+		})
+	});
 	
-	game.generateGame();
-	game.saveMemory();
-	response.sendRedirect(String.format("UNO.jsp?name=%s",request.getParameter("hostPlayer").toString().trim()));
+</script>
+</body>
 
-
-%>
+</html>
